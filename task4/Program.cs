5ebe5fc4947469fd27eb80d5fc9c5159e4f19e69
FiL <Filll707@yandex.ru>
2022-06-29 23:39:55 +0200

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число для проверки");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число для проверки");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число для проверки");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+{
+    Console.WriteLine($"Число {max} является максимальным из трех чисел {numberA}, {numberB}, {numberC}");
+}
+
+
